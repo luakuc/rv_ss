da6566bd1fd6d9ef3ff246493b4986aa3654493f
Shunsuke Mie <sux2mfgj@gmail.com>
2019-11-28 22:43:57 +0900

    change a interface of fdt module.

--- include/fdt.h
+++ include/fdt.h
@@ -12,6 +12,8 @@ typedef struct property
     char *name;
 } property_t;
 
+#include "fdt_core.h"
+
 bool init_fdt(const uint64_t fdt_base);
-uint64_t get_fdt_base(void);
+fdt_header_t* get_fdt_base(void);
 property_t *get_property(const char *node_path, const char *prop_name);
